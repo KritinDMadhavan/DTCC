e3e848e1e3fd226c0b9dbf9343fecc266f848839
Kritin D Madhavan <kritindevadasmadhavan@gmail.com>
2025-06-12 16:40:50 +0530

    init-2

--- src/pages/ISO42001AuditPage.tsx
+++ src/pages/ISO42001AuditPage.tsx
@@ -88,7 +88,7 @@ const ISO42001AuditPage: React.FC = () => {
       try {
         // Try to check for models - if successful, auto-complete certain subsections
         const modelsResponse = await axios.get(
-          `http://localhost:8000/ml/dummy-1/models/list`,
+          `https://prism-backend-dtcc-dot-block-convey-p1.uc.r.appspot.com/ml/dummy-1/models/list`,
           config
         );
 
